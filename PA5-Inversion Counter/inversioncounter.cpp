1cda7f7e8b9d5b8e6562e5c7c18224081d410699
Zack Rimshnick <87021566+zrimshnick@users.noreply.github.com>
2023-04-18 12:29:34 -0400

    Add files via upload

---
+++ PA5-Inversion Counter/inversioncounter.cpp
@@ -0,0 +1,80 @@
+/*******************************************************************************
+ * Name        : inversioncounter.cpp
+ * Author      :
+ * Version     : 1.0
+ * Date        :
+ * Description : Counts the number of inversions in an array.
+ * Pledge      :
+ ******************************************************************************/
+#include <iostream>
+#include <algorithm>
+#include <sstream>
+#include <vector>
+#include <cstdio>
+#include <cctype>
+#include <cstring>
+
+using namespace std;
+
+// Function prototype.
+static long mergesort(int array[], int scratch[], int low, int high);
+
+/**
+ * Counts the number of inversions in an array in Theta(n^2) time.
+ */
+long count_inversions_slow(int array[], int length) {
+    // TODO
+}
+
+/**
+ * Counts the number of inversions in an array in Theta(n lg n) time.
+ */
+long count_inversions_fast(int array[], int length) {
+    // TODO
+    // Hint: Use mergesort!
+}
+
+static long mergesort(int array[], int scratch[], int low, int high) {
+    // TODO
+}
+
+int main(int argc, char *argv[]) {
+    // TODO: parse command-line argument
+
+    cout << "Enter sequence of integers, each followed by a space: " << flush;
+
+    istringstream iss;
+    int value, index = 0;
+    vector<int> values;
+    string str;
+    str.reserve(11);
+    char c;
+    while (true) {
+        c = getchar();
+        const bool eoln = c == '\r' || c == '\n';
+        if (isspace(c) || eoln) {
+            if (str.length() > 0) {
+                iss.str(str);
+                if (iss >> value) {
+                    values.push_back(value);
+                } else {
+                    cerr << "Error: Non-integer value '" << str
+                         << "' received at index " << index << "." << endl;
+                    return 1;
+                }
+                iss.clear();
+                ++index;
+            }
+            if (eoln) {
+                break;
+            }
+            str.clear();
+        } else {
+            str += c;
+        }
+    }
+
+    // TODO: produce output
+
+    return 0;
+}
